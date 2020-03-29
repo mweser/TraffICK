d2753871c2fab1aad88eab1af91087c21f8dba1f
Matthew Weser <matthew.weser@gmail.com>
2020-03-28 19:42:05 -0700

    Add readme with ideas for traffic analysis

---
+++ README.md
@@ -0,0 +1,42 @@
+Traffic Bottleneck Indicators
+-> For measuring activity of traffic in normally high-volume areas
+
+Candidates:
+
+SF/Bay Area (bi-directional):
+-Bay Bridge
+-Hayward/San Mateo Bridge
+-I-280 Palo Alto-San Jose
+-SFO-Downtown
+
+Phoenix
+-101 Scottsdale-Chandler
+-I-10 between Avondale and Sky Harbor
+-I-17 corridor
+-US-51 Corridor
+
+
+DC
+-Beltway near Cabin John
+-I-66/267 Dulles-Kennedy Center
+-Baltimore-DC highways
+
+NYC
+-George Washington Bridge
+-(look for other hotspots)
+
+LA
+
+Seattle
+
+Atlanta
+
+Denver
+
+Chicago
+
+I-95 corridor
+
+
+
+
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import googlemaps
 
 def _fetch_api_key():
     with open("maps.key", 'r') as f:
-        return f.readline()
+        return googlemaps.Client(f.readline())
 
 
 def write_results(filename, contents):
@@ -14,20 +14,13 @@ def write_results(filename, contents):
         newfile.write(contents)
 
 
-gmaps = googlemaps.Client(_fetch_api_key())
+def query_trip_data(origin, dest):
+    gmaps = _fetch_api_key()
+    now = datetime.now()
+    directions_result = gmaps.directions("333 twin dolphin drive",
+                                         "555 california st",
+                                         mode="driving",
+                                         departure_time=now)
+    print(directions_result)
 
-# Geocoding an address
-# geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
 
-# Look up an address with reverse geocoding
-# reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-
-# Request directions via public transit
-now = datetime.now()
-directions_result = gmaps.directions("333 twin dolphin drive",
-                                     "555 california st",
-                                     mode="driving",
-                                     departure_time=now)
-# write_results("file_cache/syd_to_parramatta.txt", directions_result)
-
-print(directions_result)
