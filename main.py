0037b88e6b2ded007fcbba2d1740b732446c03e3
Matthew Weser <matthew.weser@gmail.com>
2020-03-28 21:14:14 -0700

    Start writing prompts for data input

--- main.py
+++ main.py
@@ -2,38 +2,57 @@ from DurationQueries import query_trip_data
 
 regions_list = set({'sf', 'la', 'ny', 'dc', 'phx'})
 
-
-def build_query_map():
-    query_map = {}
-
-def enter_new_query():
-    input_origin = input('Enter origin: ')
-    input_dest = input('Enter destination: ')
-
-
-
-def run():
-
-    start_options = """
-    1. Enter new query
-    2. View data for query
-    3. Run in background
-    """
-
-
-
-    start_operation = input('Enter new query (1) or continue with existing (2)?')
-
-    if start_operation == '1':
-        enter_new_query()
-
-    data = query_trip_data(input_origin, input_dest)
-
-
-origin = "333 twin dolphin drive"
-dest = "555 california st"
+default_origin = "333 twin dolphin drive"
+default_dest = "555 california st"
 
 other_origin = "Yamanakako Onsen Benifuji no Yu hot spring, 865-776 Yamanaka, Yamanakako, Minamitsuru District, Yamanashi 401-0501, Japan"
 other_dest = "2-chōme-13-10 Asahi, Fujiyoshida, Yamanashi 403-0012, Japan"
 
-run(other_origin, other_dest)
+
+def build_query_map():
+    query_map = {}
+
+
+def prompt_origin_dest():
+    input_origin = input('Enter origin: ')
+    input_dest = input('Enter destination: ')
+
+    return input_origin, input_dest
+
+
+def prompt_data_to_view():
+    pass
+
+
+def run_single():
+    if input('Use default origin/destination? Y/n\n') in set({'y', 'Y'}):
+        query_trip_data(default_origin, default_dest)
+    else:
+        origin, dest = prompt_origin_dest()
+        query_trip_data(origin, dest)
+
+
+def run():
+    start_options = """
+    Select an option:
+    1. Enter new query
+    2. Run single query
+    3. View data for query
+    4. Run in background
+    
+    
+    """
+
+    start_operation = input(start_options)
+
+    if start_operation == '1':
+        prompt_origin_dest()
+    elif start_operation == '2':
+        run_single()
+    elif start_operation == '3':
+        prompt_data_to_view()
+    else:
+        print('Running queries in background...')
+
+
+run()
