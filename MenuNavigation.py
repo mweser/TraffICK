0f4802164b0e5558e0d3b4c47901ca2feaa1663f
Matthew Weser <matthew.weser@gmail.com>
2020-03-28 21:32:32 -0700

    Start work on MenuNavigation

---
+++ MenuNavigation.py
@@ -0,0 +1,14 @@
+def prompt_menu(menu_list, header=""):
+    user_in = input(populate_menu(menu_list, header))
+    return menu_list[int(user_in) - 1]
+
+
+def populate_menu(menu_list, header=""):
+    output = header + "\n"
+
+    i = 0
+    for item in menu_list:
+        i += 1
+        output += "{}. {}\n".format(str(i), item)
+
+    return output
