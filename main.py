37056035aa372b487f09a6d1b82e416c3f99d37e
Matthew Weser <matthew.weser@gmail.com>
2020-03-30 00:33:16 -0700

    Clean up unused items in main.py

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-import sys
-
 from DurationQueries import query_trip_data, fetch_api_key, fetch_client
 from ImportQueries import store_csv_to_db
 from MenuNavigation import *
 from repository.DbOperations import fetch_duration_queries
-
-regions_list = set({'sf', 'la', 'ny', 'dc', 'phx'})
-
-default_origin = "333 twin dolphin drive"
-default_dest = "555 california st"
-
-other_origin = "Yamanakako Onsen Benifuji no Yu hot spring, 865-776 Yamanaka, Yamanakako, Minamitsuru District, Yamanashi 401-0501, Japan"
-other_dest = "2-chōme-13-10 Asahi, Fujiyoshida, Yamanashi 403-0012, Japan"
 
 
 def run_all_queries(client):
@@ -20,7 +10,8 @@ def run_all_queries(client):
     for duration_query in queries:
         query_trip_data(duration_query.name, duration_query.origin, duration_query.dest, client)
         if duration_query.bidirectional == '1':
-            query_trip_data(duration_query.name + '_inv', duration_query.dest, duration_query.origin, client)
+            query_trip_data(duration_query.name + '_inv', duration_query.dest,
+                            duration_query.origin, client)
 
 
 def run():
@@ -35,9 +26,6 @@ def run():
             return
 
     is_done = False
-
-
-
 
     while not is_done:
         start_options = ['Run all queries',
@@ -59,6 +47,5 @@ def run():
             sys.exit(1)
         else:
             print('Option not available (yet)')
-
 
 run()
